3e873b4f14203a37023d48f36c7a6df06a733962
rohguentak <rhgt78@gmail.com>
2021-07-02 14:40:00 +0900

    add zfs installer, version fix 4.0.10.2

--- scripts/install_iml_tar.sh
+++ scripts/install_iml_tar.sh
@@ -4,6 +4,8 @@ mkdir -p /tmp/iml-install
 cd /tmp/iml-install
 curl -L https://github.com/whamcloud/integrated-manager-for-lustre/releases/download/v4.0.10.2/iml-4.0.10.2.tar.gz | tar zx --strip 1
 yum install -y expect
+
 ./create_installer zfs
+
 curl -O https://raw.githubusercontent.com/whamcloud/integrated-manager-for-lustre/v4.0.10.2/chroma-manager/tests/utils/install.exp
 /usr/bin/expect install.exp admin "" lustre ""
